ee40a8bdb872ab8fe87d818cc9ffb9987c1cbe2b
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2026-02-18 20:59:37 -0800

    wip not tested

---
+++ src/main/java/frc/robot/subsystems/TransportSubsystem.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class TransportSubsystem extends SubsystemBase {
+  /** Creates a new TransportSubsystem. */
+  TalonFX agitatorMotorTop;
+  TalonFX agitatorMotorBottom;
+  TalonFX uptakeMotor;
+
+  public TransportSubsystem() {
+    agitatorMotorTop = new TalonFX(Constants.AGITATOR_MOTOR_TOP_ID);
+    agitatorMotorBottom = new TalonFX(Constants.AGITATOR_MOTOR_BOTTOM_ID);
+    uptakeMotor = new TalonFX(Constants.UPTAKE_MOTOR_ID);
+  }
+
+  public void agitate() {
+    // not tested
+    agitatorMotorTop.setVoltage(0.5);
+    agitatorMotorBottom.setVoltage(0.5);
+  }
+
+  public void uptake() {
+    // not tested
+    uptakeMotor.setVoltage(0.5);
+  }
+
+  public void stopAgitate() {
+    agitatorMotorTop.setVoltage(0);
+    agitatorMotorBottom.setVoltage(0);
+  }
+
+  public void stopUptake() {
+    uptakeMotor.setVoltage(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
