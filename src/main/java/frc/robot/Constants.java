2751c21a58fac0b3e6248d96bdab29541c5c554d
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2026-02-21 10:43:39 -0800

    should work, not tested. TODO: set actual motor ids, button numbers, and voltages

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,19 +7,7 @@ public class Constants {
   public static final int UPTAKE_MOTOR_ID = 23; //TODO: change to correct id
 
 
-  public static final double kPModuleTurningController = 0.5; // 0.5
-  public static final double kPModuleDriveController = 0; // added random value for test
-  public static final double kDriveEncoderDistancePerPulse = 0.0001 / 0.002706682950506; //TODO: Need to TEST
-  public static final double kMaxSpeedMetersPerSecond = 6.0; // TODO: Need to Test
-  public static final int INTAKE_MOTOR_ID = 1; // TODO: Fix when we know ID
-  public static final double IN_VOLTAGE_ID = 2; // TODO: Fix later when we find voltage
-  public static final double OUT_VOLTAGE_ID = -2; //TODO: Fix later when we find voltage
-  public static final int MOTOR_STOP_ID = 0; // to stop the motors
   public static final int LEFT_JOYSTICK_ID = 0; // TODO: Change if wrong index
   public static final int RIGHT_JOYSTICK_ID = 1; // TODO: ^^^
-  public static final int INTAKE_IN_BUTTON_NUMBER = 4; // TODO: Adjust later
-  public static final int INTAKE_OUT_BUTTON_NUMBER = -4; // TODO: Adjust later
-  public static final int SHOOTER_MOTOR_ID = 5; //TODO: Change later when we find ID
-  public static final int SHOOT_OUT_ID = 2; //TODO: Adjust when we test
      
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,40 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.AgitateCommand;
+import frc.robot.commands.TransportCommand;
+import frc.robot.commands.UptakeCommand;
+import frc.robot.subsystems.TransportSubsystem;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 
 public class RobotContainer {
+  public static Joystick rightJoystick = new Joystick(Constants.RIGHT_JOYSTICK_ID);
+  public static Joystick leftJoystick = new Joystick(Constants.LEFT_JOYSTICK_ID);
+
+  public static TransportSubsystem m_transportSubsystem = new TransportSubsystem();
+  
+  public static TransportCommand m_transportCommand = new TransportCommand(m_transportSubsystem);
+  public static AgitateCommand m_agitateCommand = new AgitateCommand(m_transportSubsystem);
+  public static UptakeCommand m_uptakeCommand = new UptakeCommand(m_transportSubsystem);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    Trigger transportButton = new JoystickButton(leftJoystick, 1);
+    Trigger agitateButton = new JoystickButton(leftJoystick, 2);
+    Trigger uptakeButton = new JoystickButton(leftJoystick, 3);
+
+    transportButton.whileTrue(m_transportCommand);
+    agitateButton.whileTrue(m_agitateCommand);  
+    uptakeButton.whileTrue(m_uptakeCommand);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
