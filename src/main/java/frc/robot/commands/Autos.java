877bc1372d5286b70f97158b99a4383d97f5ea98
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2026-03-05 20:36:24 -0800

    added comments on autos

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -41,71 +41,79 @@ public class Autos extends Command {
     m_turretSubsystem = turretSubsystem;
   }
 
+  // goes forward one meter, no vision
   public Command goStraight() {
+  // sequential does the commands one by one
     return new SequentialCommandGroup(
-        new InstantCommand(() -> m_dts.stopMotors()).withTimeout(0.1),
-        new InstantCommand(() -> m_dts.setFollowJoystick(false)).withTimeout(0.1),
+        new InstantCommand(() -> m_dts.stopMotors()).withTimeout(0.1), //stop the motors for 0.1s
+        new InstantCommand(() -> m_dts.setFollowJoystick(false)).withTimeout(0.1), //make sure we aren't moving based on joystick input
         m_dts.createPath(
-            new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(-90))),
-            new Translation2d(0, 0.5),
-            new Pose2d(0, 1, new Rotation2d(Units.degreesToRadians(-90)))),
-        new InstantCommand(() -> m_dts.setFollowJoystick(true)).withTimeout(0.1),
-        new InstantCommand(() -> m_dts.stopMotors()),
-        new InstantCommand(() -> m_dts.resetAngle(180)),
-        new InstantCommand(() -> m_dts.zeroOdometry()));
+            new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(-90))), //start of path, (0,0) with heading -90 deg
+            new Translation2d(0, 0.5), //halfway point
+            new Pose2d(0, 1, new Rotation2d(Units.degreesToRadians(-90)))), //end of path, (0,1) with heading -90 deg
+        new InstantCommand(() -> m_dts.setFollowJoystick(true)).withTimeout(0.1), //set back to using joystick input to prepare for teleop
+        new InstantCommand(() -> m_dts.stopMotors()), //stop the motors
+        new InstantCommand(() -> m_dts.resetAngle(180)), //reset the angle to 180 deg
+        new InstantCommand(() -> m_dts.zeroOdometry())); //zero the odometry
   }
 
+  //doesn't move, only shoots, no vision
   public Command middleShootAuto() {
+      // sequential does the commands one by one
     return new SequentialCommandGroup(
-        new AgitateCommand(m_transportSubsystem),
-        new WaitCommand(0.5),
-        new UptakeCommand(m_transportSubsystem),
-        new WaitCommand(0.5),
-        new ShooterCommand(m_shooterSubsystem));
+        new AgitateCommand(m_transportSubsystem), //runs the agitate command (rollers)
+        new WaitCommand(0.5), //wait 0.5s
+        new UptakeCommand(m_transportSubsystem), //runs the uptake command
+        new WaitCommand(0.5), //wait 0.5s
+        new ShooterCommand(m_shooterSubsystem)); //runs the shooter command
   }
 
+  //robot starts to the left of the hub, moves away from it 1.98m and shoots at 45 deg angle
   public Command leftShootAuto() {
+          // sequential does the commands one by one
     return new SequentialCommandGroup(
-      new InstantCommand(() -> m_dts.stopMotors()).withTimeout(0.1),
-        new InstantCommand(() -> m_dts.setFollowJoystick(false)).withTimeout(0.1),
+      new InstantCommand(() -> m_dts.stopMotors()).withTimeout(0.1),//stop the motors for 0.1s
+        new InstantCommand(() -> m_dts.setFollowJoystick(false)).withTimeout(0.1), //make sure we aren't moving based on joystick input
         m_dts.createPath(
-            new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(-90))),
-            new Translation2d(0, 0.99),
-            new Pose2d(0, 1.98, new Rotation2d(Units.degreesToRadians(-90)))),
-        new InstantCommand(() -> m_dts.setFollowJoystick(true)).withTimeout(0.1),
-        new InstantCommand(() -> m_dts.stopMotors()),
-        new InstantCommand(() -> m_dts.resetAngle(180)),
-        new InstantCommand(() -> m_dts.zeroOdometry()),
-      new AgitateCommand(m_transportSubsystem),
-      new WaitCommand(0.5),
-      new UptakeCommand(m_transportSubsystem),
-      new WaitCommand(0.5),
-      new MoveTurretToPoseCommand(m_turretSubsystem, 45), //TODO check angle
-      new WaitCommand(0.5),
-      new ShooterCommand(m_shooterSubsystem)
+            new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(-90))), //start of path, (0,0) with heading -90 deg
+            new Translation2d(0, 0.99),//halfway point
+            new Pose2d(0, 1.98, new Rotation2d(Units.degreesToRadians(-90)))),//end of path, (0,1.98) with heading -90 deg
+        new InstantCommand(() -> m_dts.setFollowJoystick(true)).withTimeout(0.1),//set back to using joystick input to prepare for teleop
+        new InstantCommand(() -> m_dts.stopMotors()),//stop the motors
+        new InstantCommand(() -> m_dts.resetAngle(180)),//reset the angle to 180 deg
+        new InstantCommand(() -> m_dts.zeroOdometry()),//zero the odometry
+      new AgitateCommand(m_transportSubsystem),//runs the agitate command (rollers)
+      new WaitCommand(0.5),//wait 0.5s
+      new UptakeCommand(m_transportSubsystem),//runs the uptake command
+      new WaitCommand(0.5),//wait 0.5s
+      new MoveTurretToPoseCommand(m_turretSubsystem, 45), //TODO check angle    turns turret to 45 deg
+      new WaitCommand(0.5),//wait 0.5s
+      new ShooterCommand(m_shooterSubsystem)//runs the shooter command
 
     );
   }
 
+  //robot starts to the right of the hub, moves away from it 1.98m and shoots at -45 deg angle
   public Command rightShootAuto() {
+    // sequential does the commands one by one
     return new SequentialCommandGroup(
-      new InstantCommand(() -> m_dts.stopMotors()).withTimeout(0.1),
-        new InstantCommand(() -> m_dts.setFollowJoystick(false)).withTimeout(0.1),
+      new InstantCommand(() -> m_dts.stopMotors()).withTimeout(0.1),//stop the motors for 0.1s
+        new InstantCommand(() -> m_dts.setFollowJoystick(false)).withTimeout(0.1),//make sure we aren't moving based on joystick input
         m_dts.createPath(
-            new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(-90))),
-            new Translation2d(0, 0.99),
-            new Pose2d(0, 1.98, new Rotation2d(Units.degreesToRadians(-90)))),
-        new InstantCommand(() -> m_dts.setFollowJoystick(true)).withTimeout(0.1),
-        new InstantCommand(() -> m_dts.stopMotors()),
-        new InstantCommand(() -> m_dts.resetAngle(180)),
-        new InstantCommand(() -> m_dts.zeroOdometry()),
-      new AgitateCommand(m_transportSubsystem),
-      new WaitCommand(0.5),
-      new UptakeCommand(m_transportSubsystem),
-      new WaitCommand(0.5),
-      new MoveTurretToPoseCommand(m_turretSubsystem, -45), //TODO check angle
-      new WaitCommand(0.5),
-      new ShooterCommand(m_shooterSubsystem)
+            new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(-90))),//start of path, (0,0) with heading -90 deg
+            new Translation2d(0, 0.99),//stop the motors
+            new Pose2d(0, 1.98, new Rotation2d(Units.degreesToRadians(-90)))),//end of path, (0,1.98) with heading -90 deg
+        new InstantCommand(() -> m_dts.setFollowJoystick(true)).withTimeout(0.1),//set back to using joystick input to prepare for teleop
+        new InstantCommand(() -> m_dts.stopMotors()),//stop the motors
+        new InstantCommand(() -> m_dts.resetAngle(180)),//reset the angle to 180 deg
+        new InstantCommand(() -> m_dts.zeroOdometry()),//zero the odometry
+      new AgitateCommand(m_transportSubsystem),//runs the agitate command (rollers)
+      new WaitCommand(0.5),//wait 0.5s
+      new UptakeCommand(m_transportSubsystem),//runs the uptake command
+      new WaitCommand(0.5),//wait 0.5s
+      new MoveTurretToPoseCommand(m_turretSubsystem, -45), //TODO check angle       turns turret to 45 deg
+      new WaitCommand(0.5),//wait 0.5s
+      new ShooterCommand(m_shooterSubsystem)//runs the shooter command
 
     );
   }
@@ -114,7 +122,7 @@ public class Autos extends Command {
 
 
 
-//---------
+//---------old
 
 
 
