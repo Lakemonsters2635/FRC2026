aca8ded7a83233f7939834481c9eb1b8152e5cee
Preston-Hegouaburu <preston.hegouaburu@gmail.com>
2026-03-09 18:12:22 -0700

    Added linear power change to shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,26 +5,35 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
+  ObjectTrackerSubsystem m_objectTrackerSubsystem;
   TalonFX m_shooterMotorLeft;
 
   TalonFX m_shooterMotorRight;
   Joystick joystick = new Joystick(0);
-  double power = 0;
+  double power = 7;
 
-  public ShooterSubsystem() {
+  public ShooterSubsystem(ObjectTrackerSubsystem objectTrackerSubsystem) {
+    m_objectTrackerSubsystem = objectTrackerSubsystem;
     m_shooterMotorLeft = new TalonFX(Constants.SHOOTER_MOTOR_ID_LEFT);
     m_shooterMotorRight = new TalonFX(Constants.SHOOTER_MOTOR_ID_RIGHT);
+    SmartDashboard.putNumber("Shooter Power", 8);
   }
 
   public void shoot() {
-    m_shooterMotorRight.setVoltage(Constants.SHOOT * -1);
-    m_shooterMotorLeft.setVoltage(Constants.SHOOT);
+    Pose2d target = m_objectTrackerSubsystem.getDistVector(0, Units.metersToInches(.6), 0, 10);
+    double magnitude = Math.sqrt(target.getX() * target.getX() + target.getY() * target.getY());
+    m_shooterMotorRight.setVoltage(((power)+(magnitude/4.5))*-1);//SmartDashboard.getNumber("Shooter Power", 8) * -1);
+    m_shooterMotorLeft.setVoltage((power+(magnitude/4.5)));SmartDashboard.getNumber("Shooter Power", 8);
   }
 
   public void shooterStop() {
